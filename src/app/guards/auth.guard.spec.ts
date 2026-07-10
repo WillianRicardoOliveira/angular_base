791276422ed637db26ecfb1ab018d78682704a8c
Willian Oliveira <willian.vancouver@gmail.com>
2026-07-10 12:35:06 -0300

    ajustes de codigo

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,15 +1,45 @@
-import {TestBed, inject, waitForAsync} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
+import {Router} from '@angular/router';
 
 import {AuthGuard} from './auth.guard';
+import {UserService} from '@services/user/user.service';
 
 describe('AuthGuard', () => {
+    const routerMock = {
+        navigate: jasmine.createSpy('navigate')
+    };
+
+    const userServiceMock = {
+        estaLogado: jasmine.createSpy('estaLogado')
+    };
+
     beforeEach(() => {
+        routerMock.navigate.calls.reset();
+        userServiceMock.estaLogado.calls.reset();
+
         TestBed.configureTestingModule({
-            providers: [AuthGuard]
+            providers: [
+                {provide: Router, useValue: routerMock},
+                {provide: UserService, useValue: userServiceMock}
+            ]
         });
     });
 
-    it('should ...', inject([AuthGuard], (guard: AuthGuard) => {
-        expect(guard).toBeTruthy();
-    }));
+    it('should allow access when user is logged in', () => {
+        userServiceMock.estaLogado.and.returnValue(true);
+
+        const result = TestBed.runInInjectionContext(() => AuthGuard());
+
+        expect(result).toBeTrue();
+        expect(routerMock.navigate).not.toHaveBeenCalled();
+    });
+
+    it('should redirect to login when user is not logged in', () => {
+        userServiceMock.estaLogado.and.returnValue(false);
+
+        const result = TestBed.runInInjectionContext(() => AuthGuard());
+
+        expect(result).toBeFalse();
+        expect(routerMock.navigate).toHaveBeenCalledWith(['/login']);
+    });
 });
