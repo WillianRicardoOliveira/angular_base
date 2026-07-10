802185826bf9b0cd0d180ac70636af913df61787
Willian Oliveira <willian.vancouver@gmail.com>
2026-07-10 09:34:05 -0300

    Clean up obsolete migration dependencies

--- src/app/modules/main/header/header.component.spec.ts
+++ src/app/modules/main/header/header.component.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {By} from '@angular/platform-browser';
 import {DebugElement} from '@angular/core';
