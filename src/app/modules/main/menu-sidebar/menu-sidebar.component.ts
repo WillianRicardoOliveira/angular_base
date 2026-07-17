2870cdc534fa6934a30f0285c3439f02c34738a9
Willian Oliveira <willian.vancouver@gmail.com>
2026-07-17 13:43:38 -0300

    Modelo de menu

--- src/app/modules/main/menu-sidebar/menu-sidebar.component.ts
+++ src/app/modules/main/menu-sidebar/menu-sidebar.component.ts
@@ -1,9 +1,9 @@
-import {AppState} from '@/store/state';
-import {UiState} from '@/store/ui/state';
-import {Component, HostBinding, OnInit} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {AppService} from '@services/app.service';
-import {Observable} from 'rxjs';
+import { AppState } from '@/store/state';
+import { UiState } from '@/store/ui/state';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppService } from '@services/app.service';
+import { Observable } from 'rxjs';
 
 const BASE_CLASSES = 'main-sidebar elevation-4';
 @Component({
@@ -18,10 +18,12 @@ export class MenuSidebarComponent implements OnInit {
     public user;
     public menu = MENU;
 
+    public menuConfiguracoes = MENU_CONFIGURACOES;
+
     constructor(
         public appService: AppService,
         private store: Store<AppState>
-    ) {}
+    ) { }
 
     ngOnInit() {
         this.ui = this.store.select('ui');
@@ -38,50 +40,116 @@ export const MENU = [
         iconClasses: 'fas fa-tachometer-alt',
         path: ['/dashboard']
     },
-    {/*
-        name: 'Main Menu',
-        iconClasses: 'fas fa-folder',
-        children: [            
+    {
+        name: 'Contabilidade',
+        iconClasses: 'far fa-list-alt',
+        children: [
             {
-                name: 'Fornecedores',
+                name: 'Menu 1',
                 iconClasses: 'fas fa-file',
-                path: ['/fornecedor']
-            },            
+                path: ['/dashboard']
+            },
+        ]
+    },
+    {
+        name: 'Controladoria',
+        iconClasses: 'fas fa-chart-line',
+        children: [
             {
-                name: 'Produtos',
+                name: 'Menu 1',
                 iconClasses: 'fas fa-file',
-                path: ['/produto']
+                path: ['/dashboard']
+            },
+        ]
+    },
+    {
+        name: 'Crédito',
+        iconClasses: 'far fa-credit-card',
+        children: [
+            {
+                name: 'Menu 1',
+                iconClasses: 'fas fa-file',
+                path: ['/dashboard']
+            },
+        ]
+    },
+    {
+        name: 'Faturamento',
+        iconClasses: 'fas fa-file-invoice-dollar',
+        children: [
+            {
+                name: 'Menu 1',
+                iconClasses: 'fas fa-file',
+                path: ['/dashboard']
+            },
+        ]
+    },
+    {
+        name: 'Financeiro',
+        iconClasses: 'fas fa-dollar-sign',
+        children: [
+            {
+                name: 'Menu 1',
+                iconClasses: 'fas fa-file',
+                path: ['/dashboard']
+            },
+        ]
+    },
+    {
+        name: 'Fiscal',
+        iconClasses: 'far fa-file-alt',
+        children: [
+            {
+                name: 'Menu 1',
+                iconClasses: 'fas fa-file',
+                path: ['/dashboard']
+            },
+        ]
+    },
+    {
+        name: 'Logística',
+        iconClasses: 'fas fa-truck',
+        children: [
+            {
+                name: 'Internacional',
+                iconClasses: 'fas fa-circle',
+                path: ['/logistica/internacional']
             },
             {
-                name: 'Compras',
+                name: 'Nacional',
+                iconClasses: 'fas fa-circle',
+                path: ['/logistica/nacional']
+            }
+        ]
+    },
+    {
+        name: 'Supply',
+        iconClasses: 'fas fa-boxes',
+        children: [
+            {
+                name: 'Menu 1',
                 iconClasses: 'fas fa-file',
-                path: ['/compra']
+                path: ['/dashboard']
             },
+        ]
+    },
+    {
+        name: 'Relatórios',
+        iconClasses: 'far fa-chart-bar',
+        children: [
             {
-                name: 'Movimentacao',
+                name: 'Menu 1',
                 iconClasses: 'fas fa-file',
-                path: ['/movimentacao']
+                path: ['/dashboard']
             },
-            {
-                name: 'Categoria-Conta',
-                iconClasses: 'fas fa-file',
-                path: ['/categoria-conta']
-            },
-            {
-                name: 'Contas a Pagar',
-                iconClasses: 'fas fa-file',
-                path: ['/contas-pagar']
-            },
-            {
-                name: 'Forma de Pagamento',
-                iconClasses: 'fas fa-file',
-                path: ['/forma-pagamento']
-            },            
-            {
-                name: 'Status do Pagamento',
-                iconClasses: 'fas fa-file',
-                path: ['/status-pagamento']
-            },            
-        ]*/
+        ]
+    }
+];
+
+export const MENU_CONFIGURACOES = [
+    {
+        name: 'Configurações',
+        iconClasses: 'fas fa-cog',
+        path: ['/configuracoes']
     }
 ];
